40d97465e86f9dd731951e78b9f77b481a88cc4e
Andrii Malesh <males.public@gmail.com>
2021-11-21 21:55:00 +0200

    Refactored.

--- RPS.js
+++ RPS.js
@@ -1,15 +1,10 @@
 // Prompt user for input
-const userInput = prompt('Choose your weapon!', '');
-// Check input case
-const userInputFormatted = userInput.toLowerCase();
-// Make sure input is a valid string
-const checkUserInput = userInputFormatted => {
-  if (
-    userInputFormatted === 'rock' ||
-    userInputFormatted === 'paper' ||
-    userInputFormatted === 'scissors'
-  ) {
-    return true;
+const userInput = () => {
+  // Also changing case if necessary
+  const userPrompt = prompt('Choose your weapon!', '').toLowerCase();
+  // Make sure input is a valid string
+  if (userPrompt === 'rock' || userPrompt === 'paper' || userPrompt === 'scissors') {
+    return userPrompt;
   } else {
     // if invalid string return error
     alert('Invalid input, please only enter: Rock, Paper or Scissors. (Case insensitive)');
@@ -17,7 +12,7 @@ const checkUserInput = userInputFormatted => {
 };
 // After string entry generate computer's choice
 const computerOutput = () => {
-  // Random number 1-3 for computers choice
+  // Random number 1-3 for computer's choice
   const num = Math.floor(Math.random() * 3) + 1;
   if (num === 1) {
     return 'rock';
@@ -30,35 +25,31 @@ const computerOutput = () => {
   }
 };
 // Compare computer vs user
-const compare = (userInputFormatted, computerOutput, draw) => {
+const compare = (userInput, computerOutput) => {
+  // Some shortcuts for variants
+  const draw =
+    (userInput === 'rock' && computerOutput === 'rock') ||
+    (userInput === 'paper' && computerOutput === 'paper') ||
+    (userInput === 'scissors' && computerOutput === 'scissors');
   if (draw) {
     alert("It's a draw!");
-  } else if (userInputFormatted === 'rock' && computerOutput === 'paper') {
+  } else if (userInput === 'rock' && computerOutput === 'paper') {
     alert('You lose! The paper covers your rock.');
-  } else if (userInputFormatted === 'rock' && computerOutput === 'scissors') {
+  } else if (userInput === 'rock' && computerOutput === 'scissors') {
     alert('You win! Your rock beats the scissors.');
-  } else if (userInputFormatted === 'paper' && computerOutput === 'rock') {
+  } else if (userInput === 'paper' && computerOutput === 'rock') {
     alert('You win! Your paper covers the rock.');
-  } else if (userInputFormatted === 'paper' && computerOutput === 'scissors') {
+  } else if (userInput === 'paper' && computerOutput === 'scissors') {
     alert('You lose! The scissors cut your paper.');
-  } else if (userInputFormatted === 'scissors' && computerOutput === 'rock') {
+  } else if (userInput === 'scissors' && computerOutput === 'rock') {
     alert('You lose! The rock beats your scissors.');
-  } else if (userInputFormatted === 'scissors' && computerOutput === 'paper') {
+  } else if (userInput === 'scissors' && computerOutput === 'paper') {
     alert('You win! Your scissors cut the paper.');
   }
 };
-
-// Some shortcuts for variants
-const draw =
-  (userInputFormatted === 'rock' && computerOutput === 'rock') ||
-  (userInputFormatted === 'paper' && computerOutput === 'paper') ||
-  (userInputFormatted === 'scissors' && computerOutput === 'scissors');
 // The game function
-const game = (userInputFormatted, computerOutput, checkUserInput) => {
-  if (checkUserInput(userInputFormatted)) {
-    return compare(userInputFormatted, computerOutput());
-  }
+const playRound = (userInput, computerOutput, compare) => {
+  compare(userInput(), computerOutput());
 };
 // Output result
-game(userInputFormatted, computerOutput, checkUserInput);
-// Sadly I don't understand why i receive undefined here
+playRound(userInput, computerOutput, compare);
